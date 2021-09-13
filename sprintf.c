a9eaf1591ebf2ae6887dffa91abe5e0037f12d10
Undiscovered-Data <chaserbrummel@gmail.com>
2021-09-13 00:51:17 -0500

    Add files via upload

---
+++ sprintf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <gmp.h>
+#include <mpfr.h>
+
+int main(void) {
+    char data[255];
+    mp_exp_t exponent = 10;
+    unsigned long in_number = 500;
+    unsigned long precision = 256;
+    mpfr_t the_sqrt;
+	mpfr_init2(the_sqrt, precision);
+
+    mpfr_sqrt_ui(the_sqrt, in_number, MPFR_RNDN);
+    //mpfr_get_str((char *) &data, &exponent, 10, precision, the_sqrt, MPFR_RNDN);
+    mpfr_sprintf((char *) data, "hello %.250Rf", &the_sqrt);
+
+    printf("The result \n \t%s\n", data);
+    //printf("The exponent \n \t%li\n", exponent);
+
+    mpfr_clear(the_sqrt);
+    mpfr_free_cache();
+}
